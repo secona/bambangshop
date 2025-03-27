af6287681154a7c20f61b907c853d3220f26d8eb
secona <77039267+secona@users.noreply.github.com>
2025-03-27 20:49:34 +0700

    feat: Implement subscribe function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -10,4 +10,11 @@ use crate::repository::subscriber::SubscriberRepository;
 pub struct NotificationService;
 
 impl NotificationService {
+    pub fn subscribe(product_type: &str, subscriber: Subscriber) -> Result<Subscriber> {
+        let product_type_upper = product_type.to_string().to_uppercase();
+        let product_type_str = product_type_upper.as_str();
+        let subscriber_result = SubscriberRepository::add(product_type_str, subscriber);
+
+        Ok(subscriber_result)
+    }
 }
