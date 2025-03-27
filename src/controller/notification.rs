cee8582b416e8eb8b6996dc720fe2ee8485b99bb
secona <77039267+secona@users.noreply.github.com>
2025-03-27 21:14:59 +0700

    fix: unsubscribe controller

--- src/controller/notification.rs
+++ src/controller/notification.rs
@@ -7,12 +7,13 @@ use crate::service::notification::NotificationService;
 
 #[post("/subscribe/<product_type>", data = "<subscriber>")]
 pub fn subscribe(product_type: &str, subscriber: Json<Subscriber>) -> Result<Created<Json<Subscriber>>> {
-    return match NotificationService::subscribe(product_type, subscriber.into_inner()) {
+    match NotificationService::subscribe(product_type, subscriber.into_inner()) {
         Ok(f) => Ok(Created::new("/").body(Json::from(f))),
         Err(e) => Err(e),
     }
 }
 
+#[post("/unsubscribe/<product_type>?<url>")]
 pub fn unsubscribe(product_type: &str, url: &str) -> Result<Json<Subscriber>> {
     match NotificationService::unsubscribe(product_type, url) {
         Ok(f) => Ok(Json::from(f)),
